f99da422e7aeb0febd7dc92d765e5b34f073e3d4
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-04-08 15:46:44 +0000

    main objectives achieved + card value matches suit color

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  function cardRandomizer(){
+    const suitsArray = ["♦","♥","♠","♣"];
+    const valuesArray = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+    let upperSuitElement = document.querySelector(".upperSuit");
+    let lowerSuitElement = document.querySelector(".lowerSuit");
+    let cardValueElement = document.querySelector(".cardValue");
+    let randomSuit = suitsArray[Math.floor(Math.random()*4)];
+    let randomCardValue = valuesArray[Math.floor(Math.random()*12)];
+
+    upperSuitElement.innerHTML = randomSuit;
+    lowerSuitElement.innerHTML = randomSuit;
+    cardValueElement.innerHTML = randomCardValue;
+
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      upperSuitElement.style.color = "red";
+      lowerSuitElement.style.color = "red";
+      cardValueElement.style.color = "red";
+    }
+  }
+
+  cardRandomizer();
 };
